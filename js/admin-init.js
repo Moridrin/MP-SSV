22e3e28f7fd45fbf59a5e7bfb2da00cf39b0d140
Jeroen Berkvens <Account@Moridrin.com>
2016-12-13 20:17:33 +0100

    working date, time and datetime pickers on profile_page_tab.

--- js/admin-init.js
+++ js/admin-init.js
@@ -4,8 +4,18 @@
 jQuery(function ($) {
     $(document).ready(function () {
         $('.datetimepicker').datetimepicker({
-            mask:'9999-19-39 29:59',
-            format:'Y-m-d H:i'
+            mask: '9999-19-39 29:59',
+            format: 'Y-m-d H:i'
+        });
+        $('.datepicker').datetimepicker({
+            timepicker:false,
+            mask: '9999-19-39',
+            format: 'Y-m-d'
+        });
+        $('.timepicker').datetimepicker({
+            datepicker: false,
+            mask: '29:59',
+            format: 'H:i'
         });
     });
 });
