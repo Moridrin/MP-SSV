2a51b42528911bf57e0d5dd8c47d8a012b3b993e
Justin <jszanto@users.noreply.github.com>
2017-06-23 12:08:57 +0200

    Reposition some things

--- template-parts/content.php
+++ template-parts/content.php
@@ -1,27 +1,29 @@
-<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-    <div class="card">
-        <div class="card-image waves-effect waves-block waves-light">
-            <?php mp_ssv_post_thumbnail(true, array('class' => 'activator banner')); ?>
-        </div>
-        <div class="card-content">
+<div class="col s12">
+    <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+        <div class="card">
+            <div class="card-image waves-effect waves-block waves-light">
+                <?php mp_ssv_post_thumbnail(true, array('class' => 'activator banner')); ?>
+            </div>
+            <div class="card-content">
             <span class="post-meta post-date">
                 <?php the_date('M j, Y') ?>
             </span>
-            <div class="post-title">
-                <?php if (is_sticky() && is_home() && !is_paged()) : ?>
-                    <span class="sticky-post">Featured</span>
-                <?php endif; ?>
-                <h3><?php echo the_title() ?></h3>
-            </div>
-            <?php $content = get_the_content(); ?>
-            <?php $content = wp_trim_words($content, 50); ?>
-            <?php echo $content ?>
+                <div class="post-title">
+                    <?php if (is_sticky() && is_home() && !is_paged()) : ?>
+                        <span class="sticky-post">Featured</span>
+                    <?php endif; ?>
+                    <h3><?php echo the_title() ?></h3>
+                </div>
+                <?php $content = get_the_content(); ?>
+                <?php $content = wp_trim_words($content, 50); ?>
+                <?php echo $content ?>
 
+            </div>
+            <div class="card-action">
+                <a href="#" title="Read More" class="read-more">
+                    Read More <i class="fa fa-long-arrow-right right"></i>
+                </a>
+            </div>
         </div>
-        <div class="card-action">
-            <a href="#" title="Read More" class="read-more">
-                Read More <i class="fa fa-long-arrow-right right"></i>
-            </a>
-        </div>
-    </div>
-</article>
+    </article>
+</div>
