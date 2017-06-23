f5644dfa294f083da9c5323d852e7262b4e03ff1
Justin <jszanto@users.noreply.github.com>
2017-06-23 10:41:17 +0200

    Restructure the homepage

--- index.php
+++ index.php
@@ -20,9 +20,6 @@ get_header() ?>
                     <?php foreach ($birthdayNames as $birthdayName): ?>
                         <h1 class="entry-title white-text valign"><?php echo $birthdayName ?></h1>
                     <?php endforeach; ?>
-                <?php else: ?>
-                    <h1 class="entry-title header-text-color valign" style="margin-top: 0; padding-top: 30px;"><?php echo get_bloginfo() ?></h1>
-                    <h3 class="entry-title header-text-color valign"><?php echo get_bloginfo('description') ?></h3>
                 <?php endif; ?>
             </div>
         </div>
@@ -30,17 +27,62 @@ get_header() ?>
 </header>
 <div id="page" class="container <?php echo is_admin_bar_showing() ? 'wpadminbar' : '' ?> large-bar">
     <div class="row">
-        <div class="col s12 <?php echo is_dynamic_sidebar() ? 'm8 l9 xl10' : '' ?>">
+        <div class="col s12 <?php echo is_dynamic_sidebar() ? 'm7 l8 xl9' : '' ?>">
+            <h1 class="entry-title center-align valign" style="margin-top: 0; padding-top: 30px;"><?php echo get_bloginfo() ?></h1>
+            <h3 class="entry-title center-align valign"><?php echo get_bloginfo('description') ?></h3>
             <div id="primary" class="content-area">
                 <main id="main" class="site-main" role="main">
                     <?php $welcomeMessage = get_theme_mod('welcome_message', ''); ?>
                     <?php if (!empty($welcomeMessage)): ?>
                         <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-                            <div class="card-panel">
                                 <?php echo $welcomeMessage ?>
-                            </div>
                         </article>
                     <?php endif; ?>
+                    <div class="col s6 m6 l6 xl3">
+                        <div class="card hover-reveal">
+                            <div class="card-image waves-effect waves-block waves-light">
+                                <img class="blurred" src="https://atsurvivalchallenge.nl/themes/laratify-octobercms-octaskin/assets/img/pages/home/slideshow/img-02.jpg" height="325">
+                            </div>
+                            <div class="card-overlay">
+                                <a href="#" class="card-url"><h3>All Terrain&rarr;</h3></a>
+                            </div>
+
+                        </div>
+                    </div>
+                    <div class="col s6 m6 l6 xl3">
+                        <div class="card hover-reveal">
+                                <div class="card-image waves-effect waves-block waves-light">
+                                    <img class="blurred" src="https://atsurvivalchallenge.nl/themes/laratify-octobercms-octaskin/assets/img/pages/home/slideshow/img-02.jpg" height="325">
+                                </div>
+                                <div class="card-overlay">
+                                    <a href="#" class="card-url"><h3>Trainings&rarr;</h3></a>
+                                </div>
+
+                        </div>
+                    </div>
+                    <div class="col s6 m6 l6 xl3">
+                        <div class="card hover-reveal">
+                            <div class="card-image waves-effect waves-block waves-light">
+                                <img class="blurred" src="https://atsurvivalchallenge.nl/themes/laratify-octobercms-octaskin/assets/img/pages/home/slideshow/img-02.jpg" height="325">
+                            </div>
+                            <div class="card-overlay">
+                                <a href="#" class="card-url"><h3>Events&rarr;</h3></a>
+                            </div>
+
+                        </div>
+                    </div>
+                    <div class="col s6 m6 l6 xl3">
+                        <div class="card hover-reveal">
+                            <div class="card-image waves-effect waves-block waves-light">
+                                <img class="blurred" src="https://atsurvivalchallenge.nl/themes/laratify-octobercms-octaskin/assets/img/pages/home/slideshow/img-02.jpg" height="325">
+                            </div>
+                            <div class="card-overlay">
+                                <a href="#" class="card-url"><h3>Photos&rarr;</h3></a>
+                            </div>
+
+                        </div>
+                    </div>
+                    <h3>Latest news</h3>
                     <?php
                     if (have_posts()) {
                         while (have_posts()) {
