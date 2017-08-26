107e10f90ee86afa7fde14e665ddd2082609371e
Justin <jszanto@users.noreply.github.com>
2017-08-26 17:44:26 +0200

    Close the img tag

--- birthday-widget.php
+++ birthday-widget.php
@@ -35,7 +35,7 @@ class ssv_birthdays extends WP_Widget
         if (!empty($birthdayNames)) {
             ?>
             <div class="parallax-container" style="height: 150px; background-color: rgba(0,0,0,0.2);">     <div class="parallax">
-            <img src="<?= get_template_directory_uri() . '/images/birthday.gif'?>"</div></div>
+            <img src="<?= get_template_directory_uri() . '/images/birthday.gif'?>"/></div></div>
             <div class="valign-wrapper" style="position: absolute; bottom: 0; width: 100%; height: 100%"> 
             <div class="valign center-align" style="width: 100%;"> 
                 <h5 class="entry-title header-text-color valign">A Very Happy Birthday To:</h5> 
