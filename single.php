478af8ed2351fe846e70148af9f7714f3fb5473f
Jeroen Berkvens <jberkvens@triptic.nl>
2017-10-09 16:21:53 +0200

    Full Screen option added.

--- single.php
+++ single.php
@@ -1,30 +1,40 @@
-<?php get_header(); ?>
-<div id="page" class="container <?php echo is_admin_bar_showing() ? 'wpadminbar' : '' ?>">
-    <div class="row">
-        <div class="col s12 <?php echo is_dynamic_sidebar() ? 'm7 l8 xxl9' : '' ?>">
-            <div id="primary" class="content-area <?php echo strpos(get_the_content(), 'class="card') === false ? 'card' : '' ?>">
-                <div class="card-image" style="">
-                    <?php if (has_post_thumbnail()): ?>
-                    <img src="<?php the_post_thumbnail_url('ssv-banner-s') ?>" style="width: auto; margin-left: auto; margin-right: auto;"/>
-                    <?php else: ?>
-                    <div class="thumbnail-placeholder"></div>
-                    <?php endif ?>
-                    <div class="card-overlay hide-on-small">
-                        <div class="page-title valign-wrapper"><h1><?= the_title() ?></h1></div>
+<?php
+get_header();
+if (isset($_GET['fullscreen']) && filter_var($_GET['fullscreen'], FILTER_VALIDATE_BOOLEAN)) {
+    the_post();
+    get_template_part('template-parts/content', 'single');
+} else {
+    ?>
+    <div id="page" class="container <?php echo is_admin_bar_showing() ? 'wpadminbar' : '' ?>">
+        <div class="row">
+            <div class="col s12 <?php echo is_dynamic_sidebar() ? 'm7 l8 xxl9' : '' ?>">
+                <div id="primary"
+                     class="content-area <?php echo strpos(get_the_content(), 'class="card') === false ? 'card' : '' ?>">
+                    <div class="card-image" style="">
+                        <?php if (has_post_thumbnail()): ?>
+                            <img src="<?php the_post_thumbnail_url('ssv-banner-s') ?>"
+                                 style="width: auto; margin-left: auto; margin-right: auto;" />
+                        <?php else: ?>
+                            <div class="thumbnail-placeholder"></div>
+                        <?php endif ?>
+                        <div class="card-overlay hide-on-small">
+                            <div class="page-title valign-wrapper"><h1><?= the_title() ?></h1></div>
+                        </div>
                     </div>
+                    <main id="main" class="site-main" role="main">
+                        <?php
+                        the_post();
+                        get_template_part('template-parts/content', 'single');
+                        ?>
+                    </main>
                 </div>
-                <main id="main" class="site-main" role="main">
-                    <?php
-                    the_post();
-                    get_template_part('template-parts/content', 'single');
-                    ?>
-                </main>
+                <?php if (comments_open() || get_comments_number()): ?>
+                    <?php comments_template(); ?>
+                <?php endif; ?>
             </div>
-            <?php if (comments_open() || get_comments_number()): ?>
-            <?php comments_template(); ?>
-            <?php endif; ?>
+            <?php get_sidebar(); ?>
         </div>
-        <?php get_sidebar(); ?>
     </div>
-</div>
-<?php get_footer(); ?>
+    <?php
+}
+get_footer();
